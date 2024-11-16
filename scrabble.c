08d3862008bfa69a137d4b3d946fe10892156f15
benedict gona <gonamtengo50@gmail.com>
2024-11-16 12:42:44 +0300

    added return

--- scrabble.c
+++ scrabble.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
+int main()
 {
     char player1[25];
     printf("player1:\n");
-    scanf("%s,player1");
-    printf("player1:%s\n",player1);
+    scanf("%s", player1);
+    printf("player1:%s\n", player1);
+    return 0;
 }
